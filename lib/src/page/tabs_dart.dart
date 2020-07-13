05b5e0a06640cdb9b5a87434b5c87b6e223ee854
kervisvasquez1993 <kervisvasquez24@gmail.com>
2020-07-13 10:05:13 -0400

    agregamos tabs para navegar

--- lib/src/page/tabs_dart.dart
+++ lib/src/page/tabs_dart.dart
@@ -4,17 +4,46 @@ class TabsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        physics: BouncingScrollPhysics(),
-        children: <Widget>[
-          Container(
-            color: Colors.red,
-          ),
-          Container(
-            color: Colors.green,
-          )
-        ],
-      ),
+      body: _Paginas(),
+      bottomNavigationBar: _Botton_navigation_bar(),
+    );
+  }
+}
+
+class _Botton_navigation_bar extends StatelessWidget {
+  const _Botton_navigation_bar({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(currentIndex: 0, items: [
+      BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline), title: Text('titulo')),
+      BottomNavigationBarItem(
+          icon: Icon(Icons.public), title: Text('Encabezado'))
+    ]);
+  }
+}
+
+class _Paginas extends StatelessWidget {
+  const _Paginas({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      physics:
+          NeverScrollableScrollPhysics(), // para bloquear el desplazamiento
+      children: <Widget>[
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.green,
+        )
+      ],
     );
   }
 }
